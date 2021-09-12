b451ba1b944b244e6cdef1dc1c7fc2bc5263c6e3
Wang Cong <1330792337@qq.com>
2021-09-12 17:14:42 +0800

    Add project files.

---
+++ WpfUsage/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfUsage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly PerformanceCounter cpuCounter;
+        private readonly PerformanceCounter memCounter;
+        private readonly PerformanceCounter memAvailableCounter;
+        private readonly Dictionary<string, PerformanceCounter> netRxCounters;
+        private readonly Dictionary<string, PerformanceCounter> netTxCounters;
+
+        private readonly string[] interfaces;
+        private int interfaceIdx = -1;
+
+        private readonly DispatcherTimer timer;
+
+        private const int KB = 1024;
+        private const int MB = KB * 1024;
+        private const int GB = MB * 1024;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            memCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            memAvailableCounter = new PerformanceCounter("Memory", "Available Bytes");
+            netRxCounters = new Dictionary<string, PerformanceCounter>();
+            netTxCounters = new Dictionary<string, PerformanceCounter>();
+
+            interfaces = PerformanceCounterCategory.GetCategories().Where(category => category.CategoryName == "Network Interface").FirstOrDefault()?.GetInstanceNames();
+            if (interfaces != null && interfaces.Length > 0)
+            {
+                Array.Sort(interfaces);
+
+                ComBoxInterfaces.ItemsSource = interfaces;
+
+                foreach (string interfaceName in interfaces)
+                {
+                    netRxCounters.Add(interfaceName, new PerformanceCounter("Network Interface", "Bytes Received/sec", interfaceName));
+                    netTxCounters.Add(interfaceName, new PerformanceCounter("Network Interface", "Bytes Sent/sec", interfaceName));
+                }
+
+                SetActiveInterface();
+            }
+
+            timer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1),
+                IsEnabled = true
+            };
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+        private void SetActiveInterface()
+        {
+            for (int i = 0; i < interfaces.Length; i++)
+            {
+                // simple check: not good enough
+                if (netRxCounters[interfaces[i]].NextValue() > 0)
+                {
+                    interfaceIdx = i;
+                    ComBoxInterfaces.SelectedIndex = i;
+                    return;
+                }
+            }
+
+            interfaceIdx = 0;
+            ComBoxInterfaces.SelectedIndex = 0;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            CpuUsage.Content = $"{cpuCounter.NextValue():F2}%";
+            MemUsage.Content = $"{memCounter.NextValue():F2}% (Available: {memAvailableCounter.NextValue() / GB:F2}GB)";
+
+            if (interfaceIdx != -1)
+            {
+                NetRxUsage.Content = FormatRxTxRate(netRxCounters[interfaces[interfaceIdx]].NextValue());
+                NetTxUsage.Content = FormatRxTxRate(netTxCounters[interfaces[interfaceIdx]].NextValue());
+            }
+        }
+
+        private static string FormatRxTxRate(float rate)
+        {
+            if (rate < KB)
+            {
+                return $"{rate:F2}B/s";
+            }
+            else if (rate < MB)
+            {
+                return $"{rate / KB:F2}KB/s";
+            }
+            else if (rate < GB)
+            {
+                return $"{rate / MB:F2}MB/s";
+            }
+            else
+            {
+                return $"{rate / GB:F2}GB/s";
+            }
+        }
+
+        private void ComBoxInterfaces_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            interfaceIdx = ComBoxInterfaces.SelectedIndex;
+        }
+    }
+}
